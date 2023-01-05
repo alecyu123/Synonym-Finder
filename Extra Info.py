f6db90204157ef94e75a656051f6c33e6fc6c7cc
Alec Yu <alecyu123@gmail.com>
2023-01-05 11:34:39 -0500

    commit 1

---
+++ Extra Info.py
@@ -0,0 +1,7 @@
+## Project 3 Information
+
+# Create a dictionary equal to the word you are comparing, "Query Word", the key being the words that you are comparing the Query word to, call it "Related Word", and the value being the number of times a word is related to the Query Word (or used in the same sentence), this being the associated number to Related Word.
+
+# Therefore our dictionaries will look like dog = {man: 5, canine: 10, bone: 30}
+
+# We use the cosine length to relate how close the vectors that represent the Query Words are together (as an angle) in the Related Word space.
